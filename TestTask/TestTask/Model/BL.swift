3ae28087422cc622f17cc32803162676fa7cf5ed
AnnaPerehrest <nyayaka23@gmail.com>
2020-08-27 11:24:53 +0300

    Install Alamofire

---
+++ TestTask/TestTask/Model/BL.swift
@@ -0,0 +1,20 @@
+//
+//  BL.swift
+//  TestTask
+//
+//  Created by Macbook Pro on 07.08.2020.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+var favorites: [String] = ["Articl1","Articl2","Articl3","Articl4","Articl5","Articl6","Articl7","Articl8","Articl9","Articl10","Articl11","Articl12"]
+
+var mostEmailed: [String] = ["Aaaaaa","Aaaaaaa","aaaaaaaaa","aaaaaaaa","Aaaaaaaa","aaaaaaa6"]
+
+var mostShared: [String] = ["Aftghssfcl1","Asthsrtgbrtth2","Arsthtrl3","Artbhrttrhrt","Arsthr","rthrtrs6","Articl7","Arsthtrthrt","rthsrgtrtr","Atrshtrshtrft10","rthtrsth11","Arthrfshtrst"]
+
+var mostViewed: [String] = ["111","2222","33333","44444","555555","6666","777777","8888888","9999","10","11","12","12","12","12","12","777777","777777","777777","777777","777777","777777","777777"]
+
---
+++ TestTask/TestTask/ViewControllers/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  TestTask
+//
+//  Created by Macbook Pro on 07.08.2020.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController {
+ 
+    public var ItemName :String = ""
+   
+  
+    
+    @IBOutlet weak var LableContent: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LableContent.text = ItemName
+        }
+    
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
