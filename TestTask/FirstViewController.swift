4da5cbf535472c348809eeb3f9a86bc369efc548
AnnaPerehrest <nyayaka23@gmail.com>
2020-08-27 10:47:54 +0300

    Initial commit

---
+++ TestTask/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestTask
+//
+//  Created by Macbook Pro on 07.08.2020.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestTask/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestTask
+//
+//  Created by Macbook Pro on 07.08.2020.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
